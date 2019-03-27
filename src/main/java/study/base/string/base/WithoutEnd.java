b7ec5f4766383ce78a408dce50175eeb6fd437a8
OdysseusLaertiades <i.pryadko88@gmail.com>
2019-03-27 02:13:49 +0200

    - created withoutEnd task and test for it - cleanup

---
+++ src/main/java/study/base/string/base/WithoutEnd.java
@@ -0,0 +1,22 @@
+package study.base.string.base;
+
+/**
+ *
+ * Given a string, return a version without the first and last char, so "Hello" yields "ell".
+ * The string length will be at least 2.
+ *
+ *
+ * withoutEnd("Hello") → "ell"
+ * withoutEnd("java") → "av"
+ * withoutEnd("coding") → "odin"
+ *
+ * Should throw NullPointerException if one of arguments is null or IllegalArgumentException if it is empty
+ * or have bad condition
+ */
+public class WithoutEnd {
+
+  public String withoutEnd(String str) {
+    return null;
+  }
+
+}
--- src/test/java/study/base/string/base/BaseStringTasksTest.java
+++ src/test/java/study/base/string/base/BaseStringTasksTest.java
@@ -4,30 +4,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TagsTest {
+class BaseStringTasksTest {
 
   private final Tags tags = new Tags();
+  private final WithoutEnd withoutEnd = new WithoutEnd();
   private final StringBuilder sb = new StringBuilder();
   private final static String TAG = "div";
   private final static String TEXT = "Lorem Ipsum";
   private final static String EMPTY_STRING = "          ";
 
   @Test
-  void markWithDivTagsTest() {
+  void tagsTest() {
     assertEquals(
         expectedContent(),
         tags.makeTags("div", "Lorem Ipsum")
     );
   }
 
-  @Test()
-  void testException() {
+  @Test
+  void testExceptionForTags() {
     assertThrows(NullPointerException.class, () -> tags.makeTags(null, TEXT));
     assertThrows(NullPointerException.class, () -> tags.makeTags(TAG, null));
     assertThrows(NullPointerException.class, () -> tags.makeTags(null, null));
     assertThrows(IllegalArgumentException.class, () -> tags.makeTags(EMPTY_STRING, TEXT));
     assertThrows(IllegalArgumentException.class, () -> tags.makeTags(TAG, EMPTY_STRING));
     assertThrows(IllegalArgumentException.class, () -> tags.makeTags(EMPTY_STRING, EMPTY_STRING));
+  }
+
+  @Test
+  void testExceptionForWithoutEnd() {
+    assertThrows(NullPointerException.class, () -> withoutEnd.withoutEnd(null));
+    assertThrows(IllegalArgumentException.class, () -> withoutEnd.withoutEnd(EMPTY_STRING));
+    assertThrows(IllegalArgumentException.class, () -> withoutEnd.withoutEnd("A"));
+  }
+
+  @Test
+  void withoutEndTest() {
+    assertEquals(
+        String.valueOf(TAG.charAt(1)),
+        withoutEnd.withoutEnd(TAG)
+    );
   }
 
   private String expectedContent() {
