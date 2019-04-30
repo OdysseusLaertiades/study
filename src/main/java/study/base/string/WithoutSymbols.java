b96b22afe830246fe15cba054c0acc02bfcf0350
Alex <chukchoid@gmail.com>
2019-04-30 18:39:07 +0300

    WithoutSymbols is Complete

--- src/main/java/study/base/string/WithoutSymbols.java
+++ src/main/java/study/base/string/WithoutSymbols.java
@@ -15,7 +15,31 @@ package study.base.string;
 public class WithoutSymbols {
 
   public String without(String toRemove, String source) {
-    return null;
-  }
 
+      toRemove.toUpperCase();
+      source.toUpperCase();
+
+      if (toRemove.charAt(0) == ' ' && !toRemove.equals(" ") || source.charAt(0) == ' '){
+          throw new IllegalArgumentException("You sentence is not exist");
+      }
+
+      String newSTr = source.replace(toRemove,"");
+
+    return newSTr;
+  }
 }
+
+/* Its my First realization of task. All code is hand writing. For example.
+
+String newSTr ="";
+      int count = 0;
+      for (int i = 0; i<source.length(); i++){
+
+          if (source.charAt(i) == toRemove.charAt(0)){
+              newSTr += source.subSequence(count, i);
+              count = i+1;
+
+          } if (i == source.length() - 1 && newSTr.length() < source.length()){
+              newSTr += source.subSequence(count, source.length());
+          }
+      }*/
