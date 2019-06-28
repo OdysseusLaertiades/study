38ccf271a1994d58a39dba845b120f7506b443eb
Alex <chukchoid@gmail.com>
2019-06-28 15:48:41 +0300

    Duplicate is complete

--- src/main/java/study/base/string/Duplicate.java
+++ src/main/java/study/base/string/Duplicate.java
@@ -4,66 +4,63 @@ package study.base.string;
  * find - method should find the letter in the string that occurs most often, if there are none - return empty string,
  * if several letters have the same number of repetitions, return one that was detected first. Ignore whitespaces and punctuation. Case sensitive
  * without - method should return source string without all duplicate letters. Ignore whitespaces and punctuation. Case sensitive
- *
+ * <p>
  * find("without") → "t"
  * find("letters") → "e"
  * find("lEtters") → "t"
  * find("the")→ ""
- *
+ * <p>
  * without("letters") → "lrs";
  * without("leTters") → "lTtrs";
  */
 
 public class Duplicate {
 
-  private String withChange;
-
-  public String find(String source) {
-    return findMostRepeatedLetter(withoutWhitespace(source));
-  }
-
-  public String without(String source) {
-    return removeDuplicateLetters(withoutWhitespace(source)) > 0 ? withChange : source;
-  }
-
-  private String withoutWhitespace (String source){
-    withChange = source.replaceAll(" ", "");
-    return withChange;
-  }
-
-  private String findMostRepeatedLetter(String byFind){
-    int count = 1;
-    int biggestCount = 0;
-    String result = "";
-
-    for (int i = 0; i < byFind.length(); i++){
-      if(!Character.toString(byFind.charAt(i)).equals("")) {
-        for (int j = i + 1; j < byFind.length(); j++){
-          if (byFind.charAt(i) == byFind.charAt(j)) {
-            count++;
-          }
-        }
-      }
-      if (count > biggestCount && count > 1){
-        result = Character.toString(byFind.charAt(i));
-        biggestCount = count;
-      }
-      count = 1;
+    public String find(String source) {
+        return findMostRepeatedLetter(withoutWhitespace(source));
     }
-    return result;
-  }
 
-  private int removeDuplicateLetters (String byRemove){
-    int  count = 0;
-    for (int i = 0; i<withChange.length(); i++){
-      if (Character.isLetter(withChange.charAt(i))) {
-        for (int j = i + 1; j < withChange.length(); j++) {
-          if (withChange.charAt(i) == withChange.charAt(j)) {
-            withChange = withChange.replaceAll(Character.toString(withChange.charAt(i)), "");
-            count++;
-          }
+    public String without(String source) {
+        return removeDuplicateLetters(source);
+    }
+
+    private String withoutWhitespace(String source) {
+        return source.replaceAll(" ", "");
+    }
+
+    private String findMostRepeatedLetter(String byFind) {
+        int count = 1;
+        int biggestCount = 0;
+        String result = "";
+
+        for (int i = 0; i < byFind.length(); i++) {
+            if (!Character.toString(byFind.charAt(i)).equals("")) {
+                for (int j = i + 1; j < byFind.length(); j++) {
+                    if (byFind.charAt(i) == byFind.charAt(j)) {
+                        count++;
+                    }
+                }
+            }
+            if (count > biggestCount && count > 1) {
+                result = Character.toString(byFind.charAt(i));
+                biggestCount = count;
+            }
+            count = 1;
         }
-      }
-    } return count;
-  }
+        return result;
+    }
+
+    private String removeDuplicateLetters(String byRemove) {
+        String result = byRemove;
+        for (int i = 0; i < byRemove.length(); i++) {
+            if (Character.isLetter(byRemove.charAt(i))) {
+                for (int j = i + 1; j < byRemove.length(); j++) {
+                    if (byRemove.charAt(i) == byRemove.charAt(j)) {
+                        result = result.replaceAll(Character.toString(byRemove.charAt(i)), "");
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
