541ebaa8a474108ac6c5bb103e28de667d807fde
OdysseusLaertiades <i.pryadko88@gmail.com>
2019-07-02 01:56:38 +0300

    - added method map to Value class - created new matrix task and test - cleanup

--- src/main/java/study/base/arrays/matrix/MatrixAverageCalculator.java
+++ src/main/java/study/base/arrays/matrix/MatrixAverageCalculator.java
@@ -3,20 +3,9 @@ package study.base.arrays.matrix;
 /**
  * Implement method that calculate arithmetic mean of all elements of the matrix.
  *
- * calculate(
- *    new int[][]{
- *         {6, 2, -6},
- *         {0, 0, 13},
- *         {-1, 6, 7},
- *         {-3, -12, 21}
- *    });  →  2.75
+ * calculate( new int[][]{ {6, 2, -6}, {0, 0, 13}, {-1, 6, 7}, {-3, -12, 21} });  →  2.75
  *
- * calculate(
- *    new int[][]{
- *         {6, 2},
- *         {1}
- *    });  →  3
- *
+ * calculate( new int[][]{ {6, 2}, {1} });  →  3
  */
 
 public class MatrixAverageCalculator {
