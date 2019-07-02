472893d6e1843caf8ef9b15c3d83e7cceac2c202
pvitaly91 <pvitaly91@gmail.com>
2019-07-02 16:23:04 +0300

    Delete tag <p> in Duplicate task

--- src/main/java/study/base/string/Duplicate.java
+++ src/main/java/study/base/string/Duplicate.java
@@ -4,12 +4,10 @@ package study.base.string;
  * find - method should find the letter in the string that occurs most often, if there are none - return empty string,
  * if several letters have the same number of repetitions, return one that was detected first. Ignore whitespaces and punctuation. Case sensitive
  * without - method should return source string without all duplicate letters. Ignore whitespaces and punctuation. Case sensitive
- * <p>
  * find("without") → "t"
  * find("letters") → "e"
  * find("lEtters") → "t"
  * find("the")→ ""
- * <p>
  * without("letters") → "lrs";
  * without("leTters") → "lTtrs";
  */
