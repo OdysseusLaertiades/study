f7461fae5198a9b32d553c585bca2fec56066b57
AlexAnishyn <chukchoid@gmail.com>
2019-07-04 15:19:21 +0300

    Corrected PositiveFinder

--- src/main/java/study/base/arrays/PositiveFinder.java
+++ src/main/java/study/base/arrays/PositiveFinder.java
@@ -1,7 +1,7 @@
 package study.base.arrays;
 
 /**
- * Implement methods which should search index of the first and last positive number in array. If there are no positive numbers, return -1.
+ * Implement methods which should search i of the first and last positive number in array. If there are no positive numbers, return -1.
  *
  * findFirstPositive(new int[]{-7, -2, 0, -4, 5, 1, -9}) →  4
  * findLastPositive(new int[]{-7, -2, 0, -4, 5, 1, -9}) →  5
@@ -11,21 +11,21 @@ package study.base.arrays;
 public class PositiveFinder {
 
   public int findFirstPositive(int[] array) {
-    int firstPositive = 0;
-    for (int index = 0; index < array.length; index++){
-      if (array[index] > 0){
-        firstPositive = index;
+    int firstPositive = -1;
+    for (int i = 0; i < array.length; i++){
+      if (array[i] > 0){
+        firstPositive = i;
         break;
       }
     }
-    return firstPositive > 0 ? firstPositive : -1;
+    return firstPositive;
   }
 
   public int findLastPositive(int[] array) {
     int lastPositive = 0;
-    for (int index = array.length-1; index > 0; index--){
-      if (array[index] > 0){
-        lastPositive = index;
+    for (int i = array.length-1; i > 0; i--){
+      if (array[i] > 0){
+        lastPositive = i;
         break;
       }
     }
