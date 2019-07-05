952a0450a00a522b3bdf57bfef02c343a3bd339c
AlexAnishyn <chukchoid@gmail.com>
2019-07-05 17:47:17 +0300

    Completed IdentityMatrixVerifier

--- src/main/java/study/base/arrays/PositiveFinder.java
+++ src/main/java/study/base/arrays/PositiveFinder.java
@@ -1,7 +1,7 @@
 package study.base.arrays;
 
 /**
- * Implement methods which should search i of the first and last positive number in array. If there are no positive numbers, return -1.
+ * Implement methods which should search index of the first and last positive number in array. If there are no positive numbers, return -1.
  *
  * findFirstPositive(new int[]{-7, -2, 0, -4, 5, 1, -9}) →  4
  * findLastPositive(new int[]{-7, -2, 0, -4, 5, 1, -9}) →  5
@@ -22,13 +22,13 @@ public class PositiveFinder {
   }
 
   public int findLastPositive(int[] array) {
-    int lastPositive = 0;
+    int lastPositive = -1;
     for (int i = array.length-1; i > 0; i--){
       if (array[i] > 0){
         lastPositive = i;
         break;
       }
     }
-    return lastPositive > 0 ? lastPositive : -1;
+    return lastPositive;
   }
 }
--- src/main/java/study/base/arrays/matrix/IdentityMatrixVerifier.java
+++ src/main/java/study/base/arrays/matrix/IdentityMatrixVerifier.java
@@ -12,7 +12,24 @@ package study.base.arrays.matrix;
 public class IdentityMatrixVerifier {
 
   public boolean verify(int[][] matrix) {
-    return false;
-  }
+    int [] [] matrixChange = matrix;
+    boolean result = false;
+    int count = 0;
 
+    if (matrixChange[0].length != matrixChange.length){
+      throw new IllegalArgumentException();
+    }
+    for (int i = 0; i<matrixChange[0].length; i++){
+      for (int j = 0; j<matrixChange.length; j++){
+        if (i == j && matrixChange [i][j] == 1)
+          count++;
+        if (i != j && matrix [i][j] != 0)
+          return false;
+      }
+    }
+    if (count == matrixChange.length){
+      result = true;
+    }
+    return result;
+  }
 }
