60b4c422a74b3f863900569b07ec32243f588a69
OdysseusLaertiades <i.pryadko88@gmail.com>
2019-07-13 02:06:00 +0300

    - added new algorithmic tasks and tests

---
+++ src/main/java/study/base/arrays/Arrays.java
@@ -0,0 +1,23 @@
+package study.base.arrays;
+
+/**
+ * Implement sort algorithms and binary search that return idx of element or -1 if element is not found
+ */
+
+public class Arrays {
+
+  public static void quickSort(int[] arr) {
+
+  }
+
+  public static void insertionSort(int[] arr) {
+  }
+
+  public static void mergeSort(int[] arr) {
+  }
+
+  public static int binarySearch(int[] sortedArray, int findMe) {
+    return -1;
+  }
+
+}
