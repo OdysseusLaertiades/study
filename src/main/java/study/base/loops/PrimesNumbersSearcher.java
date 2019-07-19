920fea408cee119e740a5b873aed29eaaf2e5fce
AlexAnishyn <chukchoid@gmail.com>
2019-07-19 15:15:34 +0300

    Refactoring of PrimesNumbersSearcher task

--- src/main/java/study/base/loops/PrimesNumbersSearcher.java
+++ src/main/java/study/base/loops/PrimesNumbersSearcher.java
@@ -1,5 +1,9 @@
 package study.base.loops;
 
+import java.util.ArrayList;
+
+import static java.lang.String.format;
+
 /**
  * Implement method that return an array of primes from the specified range from..to. If arguments are invalid, throw IllegalArgumentException
  *
@@ -12,48 +16,62 @@ package study.base.loops;
 public class PrimesNumbersSearcher {
 
     public int[] searchInRange(int from, int to) {
-        if (isFromBiggestOfTo(from, to)) {
-            throw new IllegalArgumentException("Arguments are invalid");
+        if(from < 0)
+            from = 0;
+
+        if (to < from) {
+            throw new IllegalArgumentException(format("argument 'to': %d couldn't be bigger " +
+                    "than 'from': %d", from, to));
         }
-        return searchPrimes(from, to);
+
+        if (to < 2) {
+            throw new IllegalArgumentException(format("argument 'to' should be >= 2: %d", to));
+        }
+
+        return transformListToArray(isPrimeList(from, to, identifyPrimeNumbers(from, to)));
     }
 
-    private boolean isFromBiggestOfTo(int from, int to) {
-        return to < from;
-    }
+    private boolean [] identifyPrimeNumbers (int from, int to) {
 
-    private int[] searchPrimes(int from, int to) {
-        int indexOfArray = 0;
-        int[] search = new int[countPrimes(from, to)];
-        for (int i = from; i < to; i++) {
-            if (i < 2)
-                continue;
-            if (isPrimeNumber(i)) {
-                search[indexOfArray] = i;
-                indexOfArray++;
+        boolean [] isPrime = new boolean[to + 1];
+
+        for (int i = 0; i <= to; i++) {
+            isPrime[i] = true;
+        }
+
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for (int i = 0; i * i <= to; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= to; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return search;
+        return isPrime;
     }
 
-    private int countPrimes(int from, int to) {
+    private ArrayList<Integer> isPrimeList (int from, int to, boolean [] isPrime) {
+        ArrayList <Integer> primeList = new ArrayList<>();
+        for (int i = from; i <= to; i++) {
+            if (isPrime[i]) {
+                if (i>= from && i<= to) {
+                    primeList.add(i);
+                }
+            }
+        }
+        return primeList;
+    }
+
+    private int [] transformListToArray (ArrayList<Integer> primes) {
+        int [] primesArr = new int [primes.size()];
         int count = 0;
-        for (int i = from; i < to; i++) {
-            if (i < 2)
-                continue;
-            if (isPrimeNumber(i)) {
-                count++;
-            }
-        }
-        return count;
-    }
 
-    private boolean isPrimeNumber(int number) {
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) {
-                return false;
-            }
+        for(int i : primes){
+            primesArr[count] = i;
+            count++;
         }
-        return true;
+        return primesArr;
     }
 }
