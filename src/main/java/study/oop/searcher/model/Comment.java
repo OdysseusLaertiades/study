99ac8066681a228e41146df91e11cb917abbb1c0
AlexAnishyn <chukchoid@gmail.com>
2019-08-23 16:11:09 +0300

    Transfered interface implemented from class Comment to JsonCommentService

--- src/main/java/study/oop/searcher/model/Comment.java
+++ src/main/java/study/oop/searcher/model/Comment.java
@@ -1,38 +1,9 @@
 package study.oop.searcher.model;
 
-import study.oop.searcher.service.CommentService;
-
-import java.util.ArrayList;
-
-public class Comment implements CommentService {
-    int postId = 51;
-    String id = "254";
-    String email = "Ardella@khalid.biz";
-    String body = "laboriosam voluptas aut quibusdam mollitia sunt non dolores illum fugiat ex vero nemo aperiam porro quam " +
+public class Comment {
+    public int postId = 51;
+    public String id = "254";
+    public String email = "Ardella@khalid.biz";
+    public String body = "laboriosam voluptas aut quibusdam mollitia sunt non dolores illum fugiat ex vero nemo aperiam porro quam " +
             "expedita est vel voluptatem sit voluptas consequuntur quis eligendi omnis id nisi ducimus sapiente odit quam";
-
-    ArrayList<Comment> comment = new ArrayList<Comment>();
-    ArrayList idArr = new ArrayList();
-    Comment[] comArray = new Comment[comment.size()];
-
-    public void saveComment(Comment comment) {
-        this.comment.add(comment);
-        idArr.add(id);
-    }
-
-    public void deleteComment(String id) {
-        comment.remove(idArr.indexOf(id));
-        idArr.remove(idArr.indexOf(id));
-    }
-
-    public Comment findCommentById(String id) {
-        return comment.get(idArr.indexOf(id));
-    }
-
-    public Comment[] findAllComments() {
-        for (int i = 0; i < comment.size(); i++) {
-            comArray[i] = comment.get(i);
-        }
-        return comArray;
-    }
 }
--- src/main/java/study/oop/searcher/service/JsonCommentService.java
+++ src/main/java/study/oop/searcher/service/JsonCommentService.java
@@ -2,25 +2,37 @@ package study.oop.searcher.service;
 
 import study.oop.searcher.model.Comment;
 
+import java.util.ArrayList;
+
 public class JsonCommentService implements CommentService {
+
+  ArrayList<Comment> comment = new ArrayList<Comment>();
+  ArrayList idArr = new ArrayList();
+  Comment comm = new Comment();
 
   @Override
   public void saveComment(Comment comment) {
-
+    this.comment.add(comment);
+    idArr.add(comm.id);
   }
 
   @Override
   public void deleteComment(String id) {
-
+    comment.remove(idArr.indexOf(id));
+    idArr.remove(idArr.indexOf(id));
   }
 
   @Override
   public Comment findCommentById(String id) {
-    return null;
+    return comment.get(idArr.indexOf(id));
   }
 
   @Override
   public Comment[] findAllComments() {
-    return new Comment[0];
+    Comment[] comArray = new Comment[comment.size()];
+    for (int i = 0; i < comment.size(); i++) {
+      comArray[i] = comment.get(i);
+    }
+    return comArray;
   }
 }
