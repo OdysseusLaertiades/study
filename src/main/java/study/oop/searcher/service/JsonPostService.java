bb99caa475d3cdaf7ad5910feaa574e7b988b65d
AlexAnishyn <chukchoid@gmail.com>
2019-08-29 15:32:31 +0300

    Commit for a pull correction from Igor

--- src/main/java/study/oop/searcher/service/JsonPostService.java
+++ src/main/java/study/oop/searcher/service/JsonPostService.java
@@ -2,20 +2,31 @@ package study.oop.searcher.service;
 
 import study.oop.searcher.model.Post;
 
+import java.util.ArrayList;
+
 public class JsonPostService implements PostService {
+
+  ArrayList<Post> posts = new ArrayList<Post>();
+  ArrayList idPost = new ArrayList();
+  Post pos = new Post();
 
   @Override
   public void savePost(Post post) {
-
+    posts.add(post);
+    idPost.add(pos.id);
   }
 
   @Override
   public Post findById(String id) {
-    return null;
+    return posts.get(idPost.indexOf(id));
   }
 
   @Override
   public Post[] findAllPosts() {
-    return new Post[0];
+    Post[] arrPosts = new Post[posts.size()];
+    for (int i = 0; i < posts.size(); i++) {
+      arrPosts[i] = posts.get(i);
+    }
+    return arrPosts;
   }
 }
